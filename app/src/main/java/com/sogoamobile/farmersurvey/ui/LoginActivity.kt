63de3a40fe88d554c1bf97c7ecd86d268bfdbeb5
Suge <suge.david@healthcloud.co.ke>
2022-05-07 18:07:07 +0300

    initial commit

---
+++ app/src/main/java/com/sogoamobile/farmersurvey/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.sogoamobile.farmersurvey.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sogoamobile.farmersurvey.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
